14b2c4a2b3d7f91cafeca48a0696c9704df6197c
Firealem Erko <firealemg@yahoo.com>
2019-06-03 16:44:06 -0400

    added getby id endpoint

--- api/accountsRouter.js
+++ api/accountsRouter.js
@@ -1,15 +1,38 @@
 const router = require("express").Router();
 const db = require("../data/accounts-model");
 
+//get all
 router.get("/", async (req, res) => {
   try {
-    const actions = await db.find();
-    res.status(200).json({ success: true, actions });
+    const entry = await db.find();
+    res.status(200).json({ success: true, entry });
   } catch (err) {
-    res
-      .status(500)
-      .json({ success: false, error: "The actions could not be retrived" });
+    res.status(500).json({ success: false, error: " could not be retrived" });
   }
 });
 
-module.exports = router
+//get by id
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const entry = await db.findById(id);
+
+    if (!entry) {
+      res.status(404).json({
+        error: "The entry with the specified ID does not exist"
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        entry
+      });
+    }
+  } catch (err) {
+     res.status(500).json({
+      success: false,
+      error: " could not be retrieved"
+    });
+  }
+});
+
+module.exports = router;
